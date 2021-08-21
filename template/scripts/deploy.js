303eb24bd7aa6844a31488d0fbeeb72dbf5c1ff5
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-08-22 00:32:50 +0530

    Adding functionality to copy abi

--- template/scripts/deploy.js
+++ template/scripts/deploy.js
@@ -7,8 +7,31 @@ async function main() {
     const sampleContract = await SampleContract.deploy();
 
     await sampleContract.deployed();
-    console.log(`Sample Contract deployed to address: ${sampleContract.address}`);
+    console.log("Sample Contract address:", sampleContract.address);
 
+    saveFrontendFiles(sampleContract);
+
+}
+
+function saveFrontendFiles(contract) {
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../src/abis";
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        contractsDir + "/contract-address.json",
+        JSON.stringify({ SampleContract: contract.address }, undefined, 2)
+    );
+
+    const SampleContractArtifact = artifacts.readArtifactSync("SampleContract");
+
+    fs.writeFileSync(
+        contractsDir + "/SampleContract.json",
+        JSON.stringify(SampleContractArtifact, null, 2)
+    );
 }
 
 main()
