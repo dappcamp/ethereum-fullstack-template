2935b7ff45486e83b8e33a972feca2b1da20df21
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-08-22 18:37:48 +0530

    Changing to use infura api key

--- template/hardhat.config.js
+++ template/hardhat.config.js
@@ -4,9 +4,15 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const INFURA_API_KEY = process.env.YOUR_INFURA_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.0"
+  solidity: "0.8.0",
+  networks: {
+    test_network: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
+    }
+  }
 };
