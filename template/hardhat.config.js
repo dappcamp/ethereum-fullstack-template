1604acab0300b4660ca9538259fa484c5a2c786e
shubham-kanodia <shubhamkanodia123@gmail.com>
2021-08-22 23:33:43 +0530

    Reverting back to infura

--- template/hardhat.config.js
+++ template/hardhat.config.js
@@ -4,14 +4,14 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const INFURA_API_KEY = process.env.YOUR_INFURA_API_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     test_network: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
       accounts: [`0x${WALLET_PRIVATE_KEY}`]
     }
   }
