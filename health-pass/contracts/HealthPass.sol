618df9f765a5f9e47d3681dd540510b40e0f98b2
rajagottumukkala <rajagottumukkala3@gmail.com>
2022-01-24 07:29:36 -0800

    project created

---
+++ health-pass/contracts/HealthPass.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract HealthPass is ERC1155{
+    constructor() ERC1155("") {}
+
+    
+    
+}
