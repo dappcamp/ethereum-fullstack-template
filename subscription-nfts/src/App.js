d5356f45916100ca032d7caed6d36e39d46676e8
Dylan <dylanreid7@gmail.com>
2022-05-19 18:22:28 -0500

    Set up initial frontend with header

---
+++ subscription-nfts/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="App">
+        Ethereum Fullstack Template
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ subscription-nfts/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+export default function Header() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Subscription NFTs
+              </Typography>
+              <Button color="inherit">Connect Wallet</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      );
+}
