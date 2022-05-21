3899af9ecbfc3c016d5acb5eb0ea0025f44d14bb
dsyeag <16358063+dsyeag@users.noreply.github.com>
2022-05-21 12:09:52 -0400

    Update treasurer.sol

--- template/contracts/treasurer.sol
+++ template/contracts/treasurer.sol
@@ -1,11 +1,41 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+
+//@title: treasurer.sol
+//@author: dsyeag
+//@notice: this contract accepts deposits and withdrawals,
+    //maintains user balances, and interacts with the fundmanager.sol
+    //contract.
+
 
 pragma solidity ^0.8.0;
 
-contract SampleContract {
-    event Greet(string message);
+contract treasurer {
 
-    function greet() public {
-        emit Greet("Hello World!");
+    address owner;    // current owner of the contract
+
+    mapping (address => uint) usdcByAddress;
+    mapping (address => uint) tETFByAddress;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function withdrawUSDC(uint amount) public {
+        require(owner == msg.sender);
+        msg.sender.transfer(address(this).balance);
+    }
+    function withdrawtETF(uint amount) public {
+        msg.sender.transfer(address())
+    }
+    function deposit(uint amount) public payable {
+        require(msg.value == amount);
+        usdcByAddress[msg.sender]+=amount;
+    }
+
+    function getUSDCBalance() public view returns (uint) {
+        return usdcByAddress[msg.sender];
+    }
+    function gettETFBalance() public view returns (uint) {
+        return tETFByAddress[msg.sender];
     }
 }
