0c94a38e5e6034b978a544a93abc692b76d7ccdb
dsyeag <16358063+dsyeag@users.noreply.github.com>
2022-05-21 12:53:27 -0400

    Update treasurer.sol

--- template/contracts/treasurer.sol
+++ template/contracts/treasurer.sol
@@ -9,7 +9,15 @@
 
 pragma solidity ^0.8.0;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/math/Math.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract treasurer {
+
+    using Math for uint256;
+    using SafeMath for uint256;
 
     address owner;    // current owner of the contract
 
@@ -23,16 +31,17 @@ contract treasurer {
     function withdrawUSDC(uint amount) public {
         require(owner == msg.sender);
         require(amount<=usdcByAddress(msg.sender),"You do not hold enough USDC");
-        msg.sender.transfer(address(this).balance);
+        SafeERC20.safeTransferFrom(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, address(this), msg.sender, amount);
         usdcByAddress(msg.sender)-=amount;
     }
     function withdrawtETF(uint amount) public {
         require(amount<=tETFByAddress(msg.sender),"You do not hold enough tETF.");
-        msg.sender.transfer(address());
+        SafeERC20.safeTransferFrom('ourtokenaddress', address(this), msg.sender, amount);
         tETFByAddress(msg.sender)-=amount;
     }
     function deposit(address from, uint amount) public payable {
         require(msg.value == amount);
+        SafeERC20.safeTransferFrom(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, msg.sender, address(this), amount);
         usdcByAddress[from]+=amount;
     }
 
