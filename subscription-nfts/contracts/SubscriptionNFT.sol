8530116a8275ef4e44a73edb5f6d2b0d613cdb6c
tshr-w3 <92415573+tshr-w3@users.noreply.github.com>
2022-05-22 22:00:47 -0400

    change to ERC721, add tokenId counter, token  data struct and mapping

--- subscription-nfts/contracts/SubscriptionNFT.sol
+++ subscription-nfts/contracts/SubscriptionNFT.sol
@@ -2,8 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SubscriptionNFT is ERC1155 {
+contract SubscriptionNFT is ERC721 {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    struct TokenData {
+        address creatorAddress;
+        string accessTier;
+        uint256 expiration_time;
+    }
+
+    mapping(uint256 => TokenData) private _tokenDatas;
+
+    constructor() ERC721("SubscriptionNFT", "SUB") {}
 
 }
