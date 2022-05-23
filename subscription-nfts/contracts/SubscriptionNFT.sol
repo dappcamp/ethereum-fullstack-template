d8755c6ddf6555c345cf41e156707b7bef789f54
tshr-w3 <92415573+tshr-w3@users.noreply.github.com>
2022-05-22 22:37:19 -0400

    add subscription term and issueSubscriptionNFT function

--- subscription-nfts/contracts/SubscriptionNFT.sol
+++ subscription-nfts/contracts/SubscriptionNFT.sol
@@ -12,26 +12,47 @@ contract SubscriptionNFT is ERC721 {
     Counters.Counter private _SubscriptionTemplateIds;
 
     struct TokenData {
+        uint256 subscriptionTemplateId;
         address creatorAddress;
         string accessTier;
         uint256 expirationTime;
-        uint256 subscriptionTemplateId;
     }
 
     mapping(uint256 => TokenData) private _tokenDatas;
 
-    struct subscriptionOption {
+    struct SubscriptionOption {
         string accessTier;
         uint256 price;
+        uint256 term;
     }
 
     struct SubscriptionTemplate {
         address creatorAddress;
-        subscriptionOption[] subscriptionOptions;
+        SubscriptionOption[] subscriptionOptions;
     }
 
     mapping(uint256 => SubscriptionTemplate) private _subscriptionTemplates;
 
     constructor() ERC721("SubscriptionNFT", "SUB") {}
+
+  function issueSubscriptionNFT(address recipient, uint256 subscriptionTemplateId, uint256 subscriptionOptionSelectionIndex) public returns (uint256) {
+
+    // TODO implement payment logic
+
+    _tokenIds.increment();
+
+    uint256 newTokenId = _tokenIds.current();
+
+    _mint(recipient, newTokenId);
+
+    SubscriptionOption memory selectedSubscriptionOption = _subscriptionTemplates[subscriptionTemplateId].subscriptionOptions[subscriptionOptionSelectionIndex];
+
+    _tokenDatas[newTokenId].subscriptionTemplateId = subscriptionTemplateId;
+    _tokenDatas[newTokenId].creatorAddress = _subscriptionTemplates[subscriptionTemplateId].creatorAddress;
+    _tokenDatas[newTokenId].accessTier = selectedSubscriptionOption.accessTier;
+    _tokenDatas[newTokenId].expirationTime = block.timestamp + selectedSubscriptionOption.term;
+
+    return newTokenId;
+  }
 
 }
