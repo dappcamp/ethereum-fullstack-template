23127273478ff31bb4243408f871e056466c156c
tshr-w3 <92415573+tshr-w3@users.noreply.github.com>
2022-05-23 13:42:16 -0400

    cleanup and remove creator address from token data

--- subscription-nfts/contracts/SubscriptionNFT.sol
+++ subscription-nfts/contracts/SubscriptionNFT.sol
@@ -9,11 +9,10 @@ contract SubscriptionNFT is ERC721 {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    Counters.Counter private _SubscriptionTemplateIds;
+    Counters.Counter private _subscriptionTemplateIds;
 
     struct TokenData {
         uint256 subscriptionTemplateId;
-        address creatorAddress;
         string accessTier;
         uint256 expirationTime;
     }
@@ -48,7 +47,6 @@ contract SubscriptionNFT is ERC721 {
         SubscriptionOption memory selectedSubscriptionOption = _subscriptionTemplates[subscriptionTemplateId].subscriptionOptions[subscriptionOptionSelectionIndex];
 
         _tokenDatas[newTokenId].subscriptionTemplateId = subscriptionTemplateId;
-        _tokenDatas[newTokenId].creatorAddress = _subscriptionTemplates[subscriptionTemplateId].creatorAddress;
         _tokenDatas[newTokenId].accessTier = selectedSubscriptionOption.accessTier;
         _tokenDatas[newTokenId].expirationTime = block.timestamp + selectedSubscriptionOption.term;
 
